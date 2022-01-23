8e3fc700531fc5eb4fa550473b57f35126b85323
Joseph Lundy <jclundy@github.com>
2022-01-23 14:47:36 -0500

    updates AboutMe section, removes old content

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,6 @@
     <HomeCover />
     <Navigation/>
     <AboutMe/> 
-    <Instagram/>
-    <Italki/>
-    <ContactMe/>
-    <Notice/>
     <Footer/>
 
     <div class="scroll-up">
@@ -22,10 +18,6 @@
 import HomeCover from "./sections/HomeCover.vue";
 import Navigation from "./sections/Navigation.vue";
 import AboutMe from "./sections/AboutMe.vue";
-import Instagram from "./sections/Instagram.vue";
-import Italki from "./sections/Italki.vue";
-import ContactMe from "./sections/ContactMe.vue";
-import Notice from "./sections/Notice.vue";
 import Footer from "./sections/Footer.vue";
 
 export default {
@@ -35,10 +27,6 @@ export default {
     HomeCover,
     Navigation,
     AboutMe,
-    Instagram,
-    Italki,
-    ContactMe,
-    Notice,
     Footer,
   },
 };
--- src/sections/AboutMe.vue
+++ src/sections/AboutMe.vue
@@ -1,29 +1,35 @@
 <template>
-  <div id="aboutme" class="section-padding">
-    <div class="container ">
-      <h2 class="section-title wow fadeInUp">About Me</h2>
-    </div>
-    <!-- Video Section -->
-    <section class="video-section">
-      <div class="tf-bg-overlay">
-        <div class="container">
-          <div class="video-intro">
-            <a
-              class="popup-video"
-              href="https://www.youtube.com/watch?v=hmarkUeV98c"
-            >
-              <i class="fa fa-play"></i>
-            </a>
-            <h2>Video Introducing</h2>
-          </div>
+<section id="about" class="about-section section-padding">
+      <div class="container">
+        <h2 class="section-title wow fadeInUp">About Me</h2>
+
+        <div class="short-info wow fadeInUp">
+          <h3>Objective</h3>
+          <p>I am looking for a role where I can further my knowledge of robotics and solve interesting problems</p>
         </div>
 
-        <!--/.container-->
-      </div>
-      <!--/.overlay-->
-    </section>
-    <!-- /.Video Section -->
-  </div>
+        <div class="short-info wow fadeInUp">
+          <h3>Professional Interests</h3>
+          <p>My professional interests include state estimation, sensor fusion, controls, and designing AI systems.</p>
+
+          <ul class="list-check">
+            <li>C/C++</li>
+            <li>Python</li>
+            <li>Octave/Matlab</li>
+            <li>Real-time systems</li>
+            <li>Controls</li>
+            <li>State Estimation and Sensor Fusion</li>
+            <li>SLAM</li>
+            <li>Computer Vision</li>
+          </ul>
+        </div>
+
+        <div class="download-button">
+          <a class="btn btn-info btn-lg" href="#contact"><i class="fa fa-paper-plane"></i>Send me message</a>
+          <a class="btn btn-primary btn-lg" href="#"><i class="fa fa-download"></i>download my cv</a>
+        </div>
+      </div> <!-- /.container -->
+    </section><!-- End About Section -->
 </template>
 
 <script>
